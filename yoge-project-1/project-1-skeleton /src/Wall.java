ec68c5965fab68897f3b4482d866ce3d5831eccb
YHGe020502 <139041297+YHGe020502@users.noreply.github.com>
2023-07-09 18:19:59 +1000

    Delete yoge-project-1/project-1-skeleton  directory

--- yoge-project-1/project-1-skeleton /src/Wall.java
+++
@@ -1,9 +0,0 @@
-public class Wall extends Object {
-    public Wall(double position_x, double position_y, String imageFilename) {
-        super(position_x, position_y,imageFilename);
-    }
-    public void placeObject() {
-        getOBJECT_IMAGE().drawFromTopLeft(this.getPosition_x(), this.getPosition_y());
-    }
-}
-
